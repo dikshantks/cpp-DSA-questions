e0c71e963fdd960aee95861bb3daa54e7ae15f32
dikshantks <dikshantks229@gmail.com>
2022-01-19 16:00:29 +0530

    ..

--- char_array.cpp
+++ char_array.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -15,15 +15,15 @@ int main()
     }
 */
     //input out put
- /*
+    /*
     char arr[100];
     cout<<"enter name: ";
     cin>>arr;
     cout<<arr<<endl;
    */
 
-   //PALINDROM 
-/*
+    //PALINDROM
+    /*
  int n;
    cout<<"number of letter: ";
    cin>>n;
@@ -51,59 +51,56 @@ int main()
        cout<<"word is not palindrom"<<endl;
    }*/
 
-   //LARGEST WORD IN SENTENCE
+    //LARGEST WORD IN SENTENCE
 
-   int n;
-   char arr[n+1];
-   int max =0 , current=0;
-   int st = 0 , maxst = 0 ;
+    int n;
+    char arr[n + 1];
+    int max = 0, current = 0;
+    int st = 0, maxst = 0;
 
-   cout<<"enter 10 (this  includes space and end char '/0') : ";
-   cin>>n;
-   cin.ignore();
+    cout << "enter 10 (this  includes space and end char '/0') : ";
+    cin >> n;
+    cin.ignore();
 
-   cout<<"enter sentence : ";
-   
-   cin.getline(arr,n);
-   cin.ignore();
+    cout << "enter sentence : ";
 
-   int i = 0 ;
+    cin.getline(arr, n);
+    cin.ignore();
 
+    int i = 0;
 
-   while (true)
-   {
-       if (arr[i]==' ' || arr[i] =='\0' )
-       {
-           if ( current > max)
-           {
-               max = current;
-               maxst = st;
-           }
-           current=0;
-           st = i+1 ; 
-       }
-       else
-       {
-           current++;
-       }  
-       
+    while (true)
+    {
+        if (arr[i] == ' ' || arr[i] == '\0')
+        {
+            if (current > max)
+            {
+                max = current;
+                maxst = st;
+            }
+            current = 0;
+            st = i + 1;
+        }
+        else
+        {
+            current++;
+        }
 
-       if (arr[i] == '\0')
-       {
-           break;
-       }
-       i++;
-       for (int j = 0; j <= st; j++)
-       {
-           cout<<arr[i + maxst];
-       }
-   }
-   cout<<st<<endl;
-   cout<<maxst<<endl;
+        if (arr[i] == '\0')
+        {
+            break;
+        }
+        i++;
+        for (int j = 0; j <= st; j++)
+        {
+            cout << arr[i + maxst];
+        }
+    }
+    cout << st << endl;
+    cout << maxst << endl;
 
-   cout<<arr<<endl;
-   cout<<max;
-   
+    cout << arr << endl;
+    cout << max;
 
-    return 0; 
+    return 0;
 }
