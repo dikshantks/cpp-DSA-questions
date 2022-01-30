9aec05784eb7728de73c48d0a966d911391722af
dikshantks <dikshantks229@gmail.com>
2022-01-30 17:43:29 +0530

    string Q

--- string_questions.cpp
+++ string_questions.cpp
@@ -8,7 +8,28 @@ using namespace std;
 int main()
 {
 
-    string s1 = "   ";
+    //SORT
+    string s1 = "dfsdjfxzmcxtpuyp";
+
+    sort(s1.begin(), s1.end());
+
+    cout << s1 << endl;
+
+    //UPPERCSAE AND LOWER CASE ORDER
+
+    cout << 'a' - 'A' << endl; // 32 ahead
+
+    string uc = "This IS in Upper caSe";
+
+    for (int i = 0; i < uc.size(); i++)
+    {
+        if (uc[i] >= 'a' && uc[i] <= 'z')
+        {
+            uc[i] -= 32;
+        }
+    }
+
+    cout << uc << endl;
 
     return 0;
 }
