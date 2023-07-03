3a6c76fae26b042165b46d7e51474535a766f6ef
dikshant sharma <dikshant.sharma21@st.niituniversity.in>
2023-07-03 23:52:47 +0530

    leetocde

--- leetcode/lc859.cpp
+++ leetcode/lc859.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <set>
+#include <vector>
 using namespace std;
 
 bool buddyStrings(string s, string goal)
 {
+    int n = s.length();
     if (s.length() != goal.length())
-        return false;
-
-    if (s == goal && unordered_set<char>(s.begin(), s.end()).size() < s.size())
-        return true;
     {
+        return false;
     }
+    if (s == goal && set<char>(s.begin(), s.end()).size() < goal.size())
+    {
+        return true;
+    }
+    vector<int> index;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != goal[i])
+        {
+            index.push_back(i);
+        }
+    }
+
+    if (index.size() != 2)
+    {
+        return false;
+    }
+
+    swap(s[index[0]], s[index[1]]);
+    return s == goal;
 }
 
 int main()
 {
+    cout << buddyStrings("ab", "ba") << endl;
     return 0;
 }
